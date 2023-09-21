3548d738395a9a523f561993a64dc3bd5b98d95a
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-09-22 09:03:38 +1200

    Update read-text-file.js Component

--- read-text-file.js
+++ read-text-file.js
@@ -2,16 +2,9 @@ const TEXTFILES = ["info01.txt", "info02.txt", "info03.txt", "info04.txt"];
 
 const diffy = document.querySelector(".container");
 
-fetchTextFiles(TEXTFILES)
-  .then(() => console.log(`The fetches of all Text Files were Successful`))
-  .catch((err) =>
-    console.error(`Error while fetching the Text Files - ${err}`)
-  );
-
-async function fetchTextFiles(files) {
+const fetchTextFiles = async (files) => {
   for (let file of files) {
     const response = await fetch(file);
-    
     const TxtFile = await response.text();
     const heading = document.createElement("h2");
     heading.innerHTML = file;
@@ -28,4 +21,11 @@ async function fetchTextFiles(files) {
     diffy.append(pretag);
   }
 }
+
+fetchTextFiles(TEXTFILES)
+  .then(() => console.log(`The fetches of all Text Files were Successful`))
+  .catch((err) =>
+    console.error(`Error while fetching the Text Files - ${err}`)
+  );
+
 
